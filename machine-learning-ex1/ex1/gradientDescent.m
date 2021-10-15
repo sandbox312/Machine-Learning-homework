ed428a179017eb708adfeaccdf14301e32d20bae
sandbox312 <50335441+sandbox312@users.noreply.github.com>
2021-10-15 13:51:10 +0800

    Update gradientDescent.m

--- machine-learning-ex1/ex1/gradientDescent.m
+++ machine-learning-ex1/ex1/gradientDescent.m
@@ -16,11 +16,12 @@ for iter = 1:num_iters
     % Hint: While debugging, it can be useful to print out the values
     %       of the cost function (computeCost) and gradient here.
     %
-    theta_temp = theta;
-    for j=1:length(theta)
-       theta_temp(j) = theta(j) -alpha/m*(X*theta-y)'*X(:,j); 
-    end
-    theta = theta_temp;
+%     theta_temp = theta;
+%     for j = 1 : length(theta)
+%         theta_temp(j) = theta(j) - alpha / m * (X * theta - y)' * X(:,j);
+%     end
+%     theta = theta_temp;
+    theta = theta - alpha / m * ((X * theta - y)' * X)'; % calculate theta in one equation(matrix) 
 
 
 
